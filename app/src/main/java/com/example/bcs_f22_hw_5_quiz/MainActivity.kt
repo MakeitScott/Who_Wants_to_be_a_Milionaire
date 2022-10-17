a5e12a91dbfb21ab26fe90ca3e89d19a6826022a
Scott <pietss@farmingdale.edu>
2022-10-16 23:21:02 -0400

    splash screen home screen and question layout

--- app/src/main/java/com/example/bcs_f22_hw_5_quiz/MainActivity.kt
+++ app/src/main/java/com/example/bcs_f22_hw_5_quiz/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.bcs_f22_hw_5_quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity,home_::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+
+
     }
 }
--- app/src/main/java/com/example/bcs_f22_hw_5_quiz/home_.kt
+++ app/src/main/java/com/example/bcs_f22_hw_5_quiz/home_.kt
@@ -1,11 +1,28 @@
 package com.example.bcs_f22_hw_5_quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class home_ : AppCompatActivity() {
+
+    lateinit var mbtnPlay:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        mbtnPlay = findViewById(R.id.btnPlay)
+
+
+
+        mbtnPlay.setOnClickListener { val intent = Intent(this, Q1::class.java)
+        startActivity(intent)
+        }
+
+
+
     }
 }
