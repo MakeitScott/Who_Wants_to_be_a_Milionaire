c0bb74ed182b5e0dea0f1a11c4e8866db2598ed8
Scott <pietss@farmingdale.edu>
2022-10-16 22:08:30 -0400

    initial commit app for a quiz that adds up points

---
+++ app/src/main/java/com/example/bcs_f22_hw_5_quiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bcs_f22_hw_5_quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
