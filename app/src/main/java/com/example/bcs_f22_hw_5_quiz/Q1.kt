2dd1936987bfe362a007240a35f5faa3188e080d
Scott <pietss@farmingdale.edu>
2022-10-17 12:36:41 -0400

    New one

--- app/src/main/java/com/example/bcs_f22_hw_5_quiz/Q1.kt
+++ app/src/main/java/com/example/bcs_f22_hw_5_quiz/Q1.kt
@@ -2,10 +2,18 @@ package com.example.bcs_f22_hw_5_quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Q1 : AppCompatActivity() {
+
+
+    lateinit var R1a:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_q1)
+
+
+        R1a
+
     }
 }
